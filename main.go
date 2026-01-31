6c71cb02a2d94b44c0b0adf2f02046cc9cbfea39
Bashir <tagirovbash.1r001@gmail.com>
2026-01-31 12:04:23 +0300

    postgres

--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"study/feature1"
-	"study/feature2"
 	"study/feature_postgres/simple_connection"
+	"study/feature_postgres/simple_sql"
 )
 
 func main() {
-	fmt.Println("Hello, Git!")
-	feature1.Feature1()
-	feature2.Feature2()
-	simple_connection.CheckConnection()
+	ctx := context.Background()
+
+	conn, err := simple_connection.CreateConnection(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	// передаем созданное подключение для создания таблицы
+	if err := simple_sql.CreateTable(ctx, conn); err != nil {
+		panic(err)
+	}
+
+	// передаем созданное подключение для внесения строк (rows) в таблицу
+	// if err := simple_sql.InsertRow(ctx,
+	// 	conn,
+	// 	"Обед",
+	// 	"Покушац надо",
+	// 	false,
+	// 	time.Now(),
+	// ); err != nil {
+	// 	panic(err)
+	// }
+
+	if err := simple_sql.UpdateRow(ctx, conn); err != nil {
+		panic(err)
+	}
+
+	// if err := simple_sql.DeleteRow(ctx, conn); err != nil {
+	// 	panic(err)
+	// }
+
+	fmt.Println("succeed!")
 }
